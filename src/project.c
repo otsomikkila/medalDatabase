d31305fd50376e8a9d498b1525b943a441f3710b
otsomikkila <otso.mikkila@hotmail.fi>
2025-03-27 16:52:25 +0200

    Added input validation

--- src/project.c
+++ src/project.c
@@ -3,6 +3,23 @@
 #include "string.h"
 
 //Linked list?
+//add a function to check from user input that it has the exact amount of parameters the command needs
+//use strtok for it
+
+int checkInput(int n, char* input) {
+    char inputCopy[1000];
+    strcpy(inputCopy, input);
+
+    char* cp = strtok(inputCopy, " ");
+    int count = 0;
+
+    while (cp != NULL) {
+        cp = strtok(NULL, " ");
+        count++;
+    }
+
+    return (count == n);
+}
 
 int main(void) {
 
@@ -18,9 +35,12 @@ int main(void) {
         switch (command)
         {
         case 'A':
+            if(!checkInput(2, userInput)) {
+                printf("ERROR\n");
+                continue;
+            }
+            printf("TOIMII\n");
             //initialize new country and add it to the list
-            printf("%s\n", userInput);
-            
             //initialize values
             test[0].gold = 0;
             test[0].silver = 0;
@@ -31,18 +51,38 @@ int main(void) {
             printf("ADD\n");
             continue;
         case 'M':
+            if(!checkInput(5, userInput)) {
+                printf("ERROR\n");
+                continue;
+            }
             printf("MEDAL\n");
             continue;
         case 'L':
+            if(!checkInput(1, userInput)) {
+                printf("ERROR\n");
+                continue;
+            }
             printf("LIST\n");
             continue;
         case 'W':
+            if(!checkInput(2, userInput)) {
+                printf("ERROR\n");
+                continue;
+            }
             printf("WRITE\n");
             continue;
         case 'O':
+            if(!checkInput(2, userInput)) {
+                printf("ERROR\n");
+                continue;
+            }
             printf("LOAD\n");
             continue;
         case 'Q':
+            if(!checkInput(1, userInput)) {
+                printf("ERROR\n");
+                continue;
+            }
             printf("QUIT\n");
             return 0;
         default:
