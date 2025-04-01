5a3b1b372c5c2f17c05a780153e177ab96928f9a
otsomikkila <otso.mikkila@hotmail.fi>
2025-04-01 16:45:10 +0300

    L functionality now working

--- src/project.c
+++ src/project.c
@@ -3,9 +3,12 @@
 #include "string.h"
 #include "stdlib.h"
 
-//Linked list?
 //add a function to check from user input that it has the exact amount of parameters the command needs
 //use strtok for it
+
+//last update:
+//the L method working and general stream print function
+//next: W using the printToStream
 
 int checkInput(int n, char* input) {
     char inputCopy[1000];
@@ -39,15 +42,16 @@ struct Country* addCountry(char* input, struct Country* first) {
     char* tokens[2];
     getTokens(input, tokens, 2);
     
-    int size = strlen(tokens[1]) + 1;
+    //get rid of '\n' at end of line
+    int size = strlen(tokens[1]);
+    tokens[1][size - 1] = '\0';
 
     struct Country* iterator = first;
 
     //if multiple countries in database, check there will be no duplicates
     if (iterator != NULL) {
-        printf("first was not null\n");
         while (iterator->next != NULL) {
-            printf("%s\n", iterator->name);
+            //printf("%s\n", iterator->name);
             if (strcmp(iterator->name, tokens[1]) == 0) {
                 printf("Country already in chart\n");
                 return NULL;
@@ -71,12 +75,29 @@ struct Country* addCountry(char* input, struct Country* first) {
 
     if (first != NULL) {
         iterator->next = newCountry;
-        printf("NEXT: %s", iterator->next->name);
+        //printf("NEXT: %s", iterator->next->name);
         return first;
     }
     else {
         return newCountry;
     }
+}
+
+void printToStream(struct Country* first, char* filename) {
+    FILE * fp = stdout;
+
+    if (filename != NULL) {
+        printf("filename is not null\n");
+        fp = fopen(filename, "w");
+    }
+    
+    struct Country* iterator = first; 
+    while (iterator != NULL) {
+        fprintf(fp, "%s %i %i %i\n", iterator->name, iterator->gold, iterator->silver, iterator->bronze);
+        iterator = iterator->next;
+    }
+
+    if(filename != NULL) fclose(fp);
 }
 
 int main(void) {
@@ -109,49 +130,46 @@ int main(void) {
                 break;
             }
             struct Country* result;
-            //should this be first saved to result and added afterwards??
             result = addCountry(userInput, first);
+            
             if(result) {
                 first = result;
-                printf("first: %s", first->name);
-                if(first->next) {
-                    printf("first next: %s\n", first->next->name);
-                }
-                else {
-                    printf("first next: null\n");
-                }
-            }
-            else {
-                printf("FAIL\n");
+                printf("SUCCESS\n");
             }
             break;
         case 'M':   //not implemented
             if(!checkInput(5, userInput)) {
-                continue;
+                printf("Too many arguments\n");
+                break;
             }
             printf("MEDAL\n");
             break;
-        case 'L':   //not implemented
+        case 'L':
             if(!checkInput(1, userInput)) {
-                continue;
+                printf("Too many arguments\n");
+                break;
             }
-            printf("LIST\n");
+            printToStream(first, NULL);
+            printf("SUCCESS\n");
             break;
         case 'W':   //not implemented
             if(!checkInput(2, userInput)) {
-                continue;
+                printf("Too many arguments\n");
+                break;
             }
             printf("WRITE\n");
             break;
         case 'O':   //not implemented
             if(!checkInput(2, userInput)) {
-                continue;
+                printf("Too many arguments\n");
+                break;
             }
             printf("LOAD\n");
             break;
         case 'Q':   //not fully implemented
             if(!checkInput(1, userInput)) {
-                continue;
+                printf("Too many arguments\n");
+                break;
             }
             free(first->name);
             free(first->next);
